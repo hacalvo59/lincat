7f2247458edd6f4366e74fc49d6ad3fe84561b45
hacalvo59 <info.samagointeriores@gmail.com>
2026-07-08 18:38:27 +0200

    Update vector3d.rs

--- motor/lincat_engine/core/vector3d.rs
+++ motor/lincat_engine/core/vector3d.rs
@@ -1,5 +1,3 @@
-use serde::{Serialize, Deserialize};
-
 #[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 pub struct Vector3D {
     pub x: f32,
@@ -8,22 +6,28 @@ pub struct Vector3D {
 }
 
 impl Vector3D {
+    #[inline]
     pub fn new(x: f32, y: f32, z: f32) -> Self {
         Self { x, y, z }
     }
-}
-pub fn sub(&self, other: &Vector3D) -> Vector3D {
-    Vector3D::new(self.x - other.x, self.y - other.y, self.z - other.z)
-}
 
-pub fn magnitude(&self) -> f32 {
-    (self.x*self.x + self.y*self.y + self.z*self.z).sqrt()
-}
-
-pub fn normalize(&self) -> Vector3D {
-    let mag = self.magnitude();
-    if mag == 0.0 {
-        return Vector3D::new(0.0, 0.0, 0.0);
+    #[inline]
+    pub fn sub(self, other: Self) -> Self {
+        Self::new(self.x - other.x, self.y - other.y, self.z - other.z)
     }
-    Vector3D::new(self.x / mag, self.y / mag, self.z / mag)
+
+    #[inline]
+    pub fn magnitude(self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    #[inline]
+    pub fn normalize(self) -> Self {
+        let mag = self.magnitude();
+        if mag < 1e-6 {
+            Self::new(0.0, 0.0, 0.0)
+        } else {
+            Self::new(self.x / mag, self.y / mag, self.z / mag)
+        }
+    }
 }
