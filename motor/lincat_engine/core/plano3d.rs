32d0175fc9c61c1ef71e6745f7668f6c90dfcdfb
hacalvo59 <info.samagointeriores@gmail.com>
2026-07-08 18:48:23 +0200

    Update plano3d.rs

--- motor/lincat_engine/core/plano3d.rs
+++ motor/lincat_engine/core/plano3d.rs
@@ -1,24 +1,34 @@
 use serde::{Serialize, Deserialize};
-use crate::core::point3d::Point3D;
 use crate::core::vector3d::Vector3D;
 
 #[derive(Serialize, Deserialize, Debug, Clone, Copy)]
 pub struct Plano3D {
-    pub punto: Point3D,
+    pub punto: Vector3D,
     pub normal: Vector3D,
 }
 
 impl Plano3D {
-    pub fn new(punto: Point3D, normal: Vector3D) -> Self {
-        Self { punto, normal: normal.normalize() }
+    #[inline]
+    pub fn new(punto: Vector3D, normal: Vector3D) -> Self {
+        Self {
+            punto,
+            normal: normal.normalize(),
+        }
     }
 
-    pub fn distancia_al_punto(&self, p: &Point3D) -> f32 {
-        let v = Vector3D::new(
-            p.x - self.punto.x,
-            p.y - self.punto.y,
-            p.z - self.punto.z,
-        );
-        self.normal.dot(&v)
+    #[inline]
+    pub fn distancia_al_punto(self, p: Vector3D) -> f32 {
+        let v = p.sub(self.punto);
+        v.dot(self.normal)
+    }
+
+    #[inline]
+    pub fn proyectar_punto(self, p: Vector3D) -> Vector3D {
+        let d = self.distancia_al_punto(p);
+        Vector3D::new(
+            p.x - self.normal.x * d,
+            p.y - self.normal.y * d,
+            p.z - self.normal.z * d,
+        )
     }
 }
